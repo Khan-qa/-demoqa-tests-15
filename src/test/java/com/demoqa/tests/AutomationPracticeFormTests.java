fbf4a032765c6f089210ead6103024c7d4bf36ec
KKaiyrbekov <han_umag@mail.com>
2022-12-26 01:23:14 +0600

    home work qa_guru_15_9

--- src/test/java/com/demoqa/tests/AutomationPracticeFormTests.java
+++ src/test/java/com/demoqa/tests/AutomationPracticeFormTests.java
@@ -1,21 +1,13 @@
 package com.demoqa.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.demoqa.pages.AutomationPracticeFormPage;
 import com.demoqa.utils.GenerateFakeData;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class AutomationPracticeFormTests {
+public class AutomationPracticeFormTests extends BaseTestConfiguration {
 
     AutomationPracticeFormPage automationPracticeFormPage = new AutomationPracticeFormPage();
     GenerateFakeData fakeData = new GenerateFakeData();
-
-    @BeforeAll
-    static void configure() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-    }
 
     @Test
     void firstTest() {
